b51f578e9ae8db57d5d209d0fbe5dfaf33cbd3a6
Taylor Kennedy <83476446+DemiEarth@users.noreply.github.com>
2026-04-25 12:32:11 -0400

    Finalized Blockout + Slight UI Fixes

--- Assets/Scripts/CutsceneToGame.cs
+++ Assets/Scripts/CutsceneToGame.cs
@@ -6,7 +6,7 @@ public class CutsceneToGame : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SceneManager.LoadScene("HubWorld");
+        SceneManager.LoadScene("HubWorld", LoadSceneMode.Single);
     }
 
     // Update is called once per frame
